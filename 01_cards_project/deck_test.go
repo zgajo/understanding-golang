8cddc7f476aa88fc40415b53ef8f85a45d11de79
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-21 23:27:17 +0100

    Testing IO file

--- 01_cards_project/deck_test.go
+++ 01_cards_project/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -10,4 +13,24 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of spades, but got %v", d[0])
+	}
+
+}
+
+func TestSaveToDeckAndTestNewDeckFromFile(t *testing.T) {
+	testingFile := "_desting.txt"
+	os.Remove(testingFile)
+
+	d := newDeck()
+	d.savetoFile(testingFile)
+
+	loadedDeck := newDeckFromFile(testingFile)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	os.Remove(testingFile)
 }

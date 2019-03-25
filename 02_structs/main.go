c77244be45e391453a72f2ac0db803a1a61b0b07
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-25 23:00:09 +0100

    Embedding structs

--- 02_structs/main.go
+++ 02_structs/main.go
@@ -1,14 +1,29 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// darko := person{"Darko", "Pranjic"}
 	// darko := person{firstName: "Darko", lastName: "Pranjic"}
 	// fmt.Println(darko)
-	var darko person
-	// fmt.Println(darko)
+	var darko = person{
+		firstName: "Darko",
+		lastName:  "Pranjic",
+		contact: contactInfo{
+			email:   "darkopranjic.4@gmail.com",
+			zipCode: 52210,
+		},
+	}
+	fmt.Println(darko)
 }

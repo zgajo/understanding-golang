1cb3cb3569eb7364cd940c8943e57c363f490b58
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-25 22:56:28 +0100

    Creating structs

---
+++ 02_structs/main.go
@@ -0,0 +1,14 @@
+package main
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	// darko := person{"Darko", "Pranjic"}
+	// darko := person{firstName: "Darko", lastName: "Pranjic"}
+	// fmt.Println(darko)
+	var darko person
+	// fmt.Println(darko)
+}

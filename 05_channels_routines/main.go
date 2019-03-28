343036c7527fe22d1ead5455ee63efdcb9748cb4
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-28 16:01:21 +0100

    Implement channel

--- 05_channels_routines/main.go
+++ 05_channels_routines/main.go
@@ -9,21 +9,26 @@ import (
 func main() {
 	links := []string{"http://google.com", "http://facebook.com", "http://golang.org"}
 
+	c := make(chan string)
+
 	for _, website := range links {
 		fmt.Println(website)
-		go checkLink(website)
+		go checkLink(website, c)
 	}
+
+	fmt.Println(<-c) // blocking line of code, waits for the message
 }
 
-func checkLink(link string) {
-
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		os.Exit(1)
 	}
 
+	c <- "Received" // send message to channel
 	fmt.Println(link, "is up!")
 
 }
